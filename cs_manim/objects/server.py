8ba5f94b96401758c4d0bc7572054c1b7efe5370
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-07-12 11:32:48 -0400

    Better server

--- cs_manim/objects/server.py
+++ cs_manim/objects/server.py
@@ -1,4 +1,4 @@
-from manim import DOWN, GREEN, LEFT, RED, UP, YELLOW, Circle, Rectangle, Text, VGroup
+from manim import DOWN, LEFT, RIGHT, UP, Circle, Rectangle, Text, VGroup
 
 from ..style import FONT_NAME, SERVER_COLOR
 
@@ -19,31 +19,32 @@ class Server(VGroup):
         Returns:
             VGroup: The group of objects representing the server
         """
+        LIGHT_RADIUS = 0.03
+
+        def create_light():
+            return Circle(radius=LIGHT_RADIUS, color=color, fill_opacity=1)
+
+        def create_rack():
+            return VGroup(
+                Rectangle(width=0.9, height=0.25, color=color, fill_opacity=0.1),
+                create_light().shift(LEFT * 0.35),
+                create_light().shift(LEFT * 0.25),
+                create_light().shift(LEFT * 0.15),
+                Rectangle(width=0.3, height=0.05, color=color, fill_opacity=1).shift(
+                    RIGHT * 0.2
+                ),
+            )
+
         return super().__init__(
             # Main case
-            Rectangle(width=1.0, height=1.4, color=color, fill_opacity=0.3),
+            Rectangle(width=1.0, height=1.25, color=color, fill_opacity=0.3),
             # Top rack
-            Rectangle(width=0.8, height=0.3, color=color, fill_opacity=0.1).shift(
-                UP * 0.4
-            ),
-            Circle(radius=0.02, color=RED, fill_opacity=1).shift(UP * 0.4 + LEFT * 0.3),
-            Circle(radius=0.02, color=GREEN, fill_opacity=1).shift(
-                UP * 0.4 + LEFT * 0.2
-            ),
+            create_rack().shift(UP * 0.45),
             # Middle rack
-            Rectangle(width=0.8, height=0.3, color=color, fill_opacity=0.1),
-            Circle(radius=0.02, color=GREEN, fill_opacity=1).shift(LEFT * 0.3),
-            Circle(radius=0.02, color=GREEN, fill_opacity=1).shift(LEFT * 0.2),
+            create_rack().shift(UP * 0.15),
+            create_rack().shift(DOWN * 0.15),
             # Bottom rack
-            Rectangle(width=0.8, height=0.3, color=color, fill_opacity=0.1).shift(
-                DOWN * 0.4
-            ),
-            Circle(radius=0.02, color=YELLOW, fill_opacity=1).shift(
-                DOWN * 0.4 + LEFT * 0.3
-            ),
-            Circle(radius=0.02, color=GREEN, fill_opacity=1).shift(
-                DOWN * 0.4 + LEFT * 0.2
-            ),
+            create_rack().shift(DOWN * 0.45),
             # Server name
-            Text(name, font_size=14, color=color, font=FONT_NAME).shift(DOWN * 0.9),
+            Text(name, font_size=14, color=color, font=FONT_NAME).shift(DOWN * 0.8),
         )
